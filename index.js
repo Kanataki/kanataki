07d28f9180906fbf71f743bcca1ca818d51d6a3a
Kanataki <gichiniwaruingi@gmail.com>
2019-04-27 19:43:05 +0300

    added cool custom cli spinner

--- index.js
+++ index.js
@@ -1,57 +1,75 @@
 #!/usr/bin/env node
+
 "use strict";
 
 var inquirer = require("inquirer");
 var chalk = require("chalk");
 var figlet = require("figlet");
+const ora = require('ora');
+
+const throbber = ora({
+    text: 'Tinkering with all the Zoinkys',
+    spinner: {
+        frames: ['😎', '🧐', '🤓', '😊'],
+        interval: 300, // Optional
+    },
+}).start();
 
 var response = chalk.bold.green;
 
-var resume = require("./resume.json");
+var coolresume = require("./coolresume.json");
 
-var resumePrompts = {
-  type: "list",
-  name: "resumeOptions",
-  message: "What shall I enlighten you about meself?",
-  choices: [...Object.keys(resume), "Exit"]
+var coolresumePrompts = {
+    type: "list",
+    name: "coolresumeOptions",
+    message: "What shall I enlighten you about meself?",
+    choices: [...Object.keys(coolresume), "Exit"]
 };
 
 function main() {
-  console.log(
-    chalk.yellow (
-      figlet.textSync('KANATAKI', { horizontalLayout: 'full'}))
-    );
-  console.log("Howdy, I am Kanataki and welcome to my resume!");
-  resumeHandler();
+    // Simulating some asynchronous work for 10 seconds...
+    setTimeout(() => {
+        throbber.stop();
+        menu()
+    }, 1000 * 10);
 }
 
-function resumeHandler() {
-  inquirer.prompt(resumePrompts).then(answer => {
-    if (answer.resumeOptions == "Exit") {
-      return;
-    }
-    var option = answer.resumeOptions;
-    console.log(response("--------------------------------------"));
-    resume[`${option}`].forEach(info => {
-      console.log(response("|   => " + info));
-    });
-    console.log(response("--------------------------------------"));
-    // console.log(resume[`${option}`]);
-    inquirer
-      .prompt({
-        type: "list",
-        name: "exitBack",
-        message: "Go back or Exit?",
-        choices: ["Back", "Exit"]
-      })
-      .then(choice => {
-        if (choice.exitBack == "Back") {
-          resumeHandler();
-        } else {
-          return;
+function menu() {
+    console.log(
+        chalk.yellow(
+            figlet.textSync('Kanataki', { horizontalLayout: 'full' }))
+    );
+    console.log("Howdy, I am Kanataki and welcome to my cool resume!");
+    coolresumeHandler();
+}
+
+function coolresumeHandler() {
+    inquirer.prompt(coolresumePrompts).then(answer => {
+        if (answer.coolresumeOptions == "Exit") {
+            return;
         }
-      });
-  });
+        var option = answer.coolresumeOptions;
+        console.log(response("--------------------------------------"));
+        coolresume[`${option}`].forEach(info => {
+            console.log(response("|   => " + info));
+        });
+        console.log(response("--------------------------------------"));
+        // console.log(resume[`${option}`]);
+        inquirer
+            .prompt({
+                type: "list",
+                name: "exitBack",
+                message: "Go back or Exit?",
+                choices: ["Back", "Exit"]
+            })
+            .then(choice => {
+                if (choice.exitBack == "Back") {
+                    coolresumeHandler();
+                } else {
+                    return;
+                }
+            });
+    });
 }
 
 main();
